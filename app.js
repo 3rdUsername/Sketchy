218b72be21b5a5ff9cdc3e46496d4b62dd7baaee
Shiwanshu-K <42643185+3rdUsername@users.noreply.github.com>
2023-12-31 14:33:44 +0530

    merged in the bug fix

--- app.js
+++ app.js
@@ -1,121 +1,97 @@
+"use strict";
 const canvas = document.getElementById("sketchCanvas");
 const ctx = canvas.getContext("2d");
 let isDrawing = false;
-let curveNo=0;
-let drawingPath = []; // Store the current drawing path
-let drawingHistory = []; // Store all drawing paths
+let curveNo = 0;
+let drawingPath = []; // Explicitly type the path array
+let drawingHistory = []; // Type the history array
 let offsetX = 0;
 let offsetY = 0;
 let topY = 0;
-// Check if the device supports touch events
-const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-
-
-
+// Check for touch support using type guards
+const isTouchDevice = (() => {
+    return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.maxTouchPoints > 0;
+})();
 // Set canvas size
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-// Event listeners
+// Event listeners with type annotations
 canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", stopDrawing);
 canvas.addEventListener("mouseout", stopDrawing);
-if (isTouchDevice) {
-    // If it's a touch device, use the 'wheel' event for touchpad scrolling
-    canvas.addEventListener('wheel', scrollCanvas);
-} else {
-    // If it's not a touch device, use the 'mousewheel' event for mouse wheel scrolling
-    canvas.addEventListener('mousewheel', scrollCanvas);
-}
-
-// Drawing functions
+canvas.addEventListener("wheel", scrollCanvas);
+// Drawing functions with type annotations
 function startDrawing(e) {
-	isDrawing = true;
-	draw(e);
+    isDrawing = true;
+    draw(e);
 }
-
 function draw(e) {
-	if (!isDrawing) return;
+    if (!isDrawing)
+        return;
     const x = e.clientX - offsetX;
     const y = e.clientY - offsetY;
-    
-    drawingPath.push({ x, y ,curveNo});
-
+    drawingPath.push({ x, y, curveNo });
     ctx.lineWidth = 5;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = '#000';
-
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "#000";
     ctx.lineTo(x, y);
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(x, y);
 }
-
 function stopDrawing() {
-	if (isDrawing) {
-		isDrawing = false;
-        curveNo+=1;
-		drawingHistory.push(drawingPath);
-		drawingPath = [];
-	}
-	ctx.beginPath();
+    if (isDrawing || !drawingPath) {
+        isDrawing = false;
+        curveNo += 1;
+        drawingHistory.push(drawingPath);
+        drawingPath = [];
+    }
+    ctx.beginPath();
 }
-
-
-
 function scrollCanvas(event) {
-    //stopDrawing()
-    const delta = event.deltaY ;
-    if (delta > 0 || topY <0) {
-        debugger;
-        // Adjust the offsetY based on the scroll direction
+    const delta = event.deltaY;
+    if (delta > 0 || topY < 0) {
         offsetY -= delta;
-        topY-=delta;
-        canvas.height +=Math.abs(delta);
+        topY -= delta;
+        canvas.height += Math.abs(delta);
         redrawCanvas();
-    }else {
-        topY=0;
+    }
+    else {
+        topY = 0;
     }
 }
-
-// Resize canvas on window resize
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     redrawCanvas();
 });
-
-
-// Undo functionality
 function undo() {
-	if (drawingHistory.length > 0) {
-        curveNo-=1;
-		drawingHistory.pop(); // Remove the last drawn path
-		redrawCanvas();
-	}
+    if (drawingHistory.length > 0) {
+        curveNo -= 1;
+        drawingHistory.pop();
+        redrawCanvas();
+    }
 }
-
-// Redraw the canvas based on the drawing history
 function redrawCanvas() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawingHistory.forEach((path) => {
-		ctx.moveTo(path[0].x, path[0].y);
-		for (let i = 1; i < path.length; i++) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawingHistory.forEach((path) => {
+        for (let i = 0; i < path.length; i++) {
+            let point = path[i];
+            let currCurveNum = point.curveNo;
             ctx.lineWidth = 5;
-            ctx.lineCap = 'round';
-            ctx.strokeStyle = '#000';
-			ctx.lineTo(path[i].x, path[i].y);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.moveTo(path[i].x, path[i].y);
-		}
-	});
+            ctx.lineCap = "round";
+            ctx.strokeStyle = "#000";
+            ctx.lineTo(path[i].x, path[i].y);
+            ctx.stroke();
+            ctx.beginPath();
+            if (i != path.length - 1)
+                ctx.moveTo(path[i].x, path[i].y);
+        }
+    });
 }
-
-// Event listener for Ctrl + Z (Windows) or Cmd + Z (Mac) keyboard shortcut
 document.addEventListener("keydown", (event) => {
-	if ((event.ctrlKey || event.metaKey) && event.key === "z") {
-		undo();
-	}
+    if ((event.ctrlKey || event.metaKey) && event.key === "z") {
+        undo();
+    }
 });
---
+++ src/app.ts
@@ -0,0 +1,117 @@
+// Declare types for clarity and type safety
+interface DrawingPoint {
+	x: number;
+	y: number;
+	curveNo: number;
+}
+
+const canvas = document.getElementById("sketchCanvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+let isDrawing = false;
+let curveNo = 0;
+let drawingPath: DrawingPoint[] = []; // Explicitly type the path array
+let drawingHistory: DrawingPoint[][] = []; // Type the history array
+let offsetX = 0;
+let offsetY = 0;
+let topY = 0;
+
+// Check for touch support using type guards
+const isTouchDevice = (() => {
+	return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.maxTouchPoints > 0;
+})();
+
+// Set canvas size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Event listeners with type annotations
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stopDrawing);
+canvas.addEventListener("mouseout", stopDrawing);
+canvas.addEventListener("wheel", scrollCanvas);
+
+
+// Drawing functions with type annotations
+function startDrawing(e: MouseEvent) {
+	isDrawing = true;
+	draw(e);
+}
+
+function draw(e: MouseEvent) {
+	if (!isDrawing ) return;
+	const x = e.clientX - offsetX;
+	const y = e.clientY - offsetY;
+
+	drawingPath.push({ x, y, curveNo });
+
+	ctx.lineWidth = 5;
+	ctx.lineCap = "round";
+	ctx.strokeStyle = "#000";
+
+	ctx.lineTo(x, y);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.moveTo(x, y);
+}
+
+function stopDrawing() {
+	if (isDrawing || !drawingPath) {
+		isDrawing = false;
+		curveNo += 1;
+    drawingHistory.push(drawingPath);
+		drawingPath = [];
+	}
+	ctx.beginPath();
+}
+
+function scrollCanvas(event: WheelEvent) {
+	const delta = event.deltaY;
+	if (delta > 0 || topY < 0) {
+		offsetY -= delta;
+		topY -= delta;
+		canvas.height += Math.abs(delta);
+		redrawCanvas();
+	} else {
+		topY = 0;
+	}
+}
+
+window.addEventListener("resize", () => {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	redrawCanvas();
+});
+
+function undo() {
+	if (drawingHistory.length > 0) {
+		curveNo -= 1;
+		drawingHistory.pop();
+		redrawCanvas();
+	}
+}
+
+function redrawCanvas() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawingHistory.forEach((path) => {
+		for (let i = 0; i < path.length; i++) {
+      let point = path[i];
+		  let currCurveNum = point.curveNo;
+			ctx.lineWidth = 5;
+			ctx.lineCap = "round";
+			ctx.strokeStyle = "#000";
+			ctx.lineTo(path[i].x, path[i].y);
+			ctx.stroke();
+			ctx.beginPath();
+			if(i!=path.length-1)
+        ctx.moveTo(path[i].x, path[i].y);
+		}
+	});
+}
+
+document.addEventListener("keydown", (event: KeyboardEvent) => {
+	if ((event.ctrlKey || event.metaKey) && event.key === "z") {
+		undo();
+	}
+});
